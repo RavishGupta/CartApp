8df4191be51c6f2d69078735b6d4a0d3ffaa590d
RavishGupta <ravishgupta2004.coder@gmail.com>
2022-11-20 16:36:42 +0530

    modify setstate

--- src/Cartitem.js
+++ src/Cartitem.js
@@ -6,12 +6,32 @@ class Cartitem extends React.Component {
         this.state={
             price:999,
             title: 'Phone',
-            qty:1,
+            qty:0,
             img:''
         }
     }
     increaseQuanity = () =>{
-
+        console.log('this', this.state);
+        // set state form 1
+        // this.setState({
+        //     qty: this.state.qty+1
+        // });
+        // set state form 2 if prev state is req then use this 
+        this.setState((prevState)=> {
+            return {
+                qty: prevState.qty+1
+            }
+        });
+    }
+    decreaseQuantity=()=>{
+        const {qty} = this.state;
+        if (qty==0){
+            return;
+        }
+        console.log('this', this.state);
+        this.setState({
+            qty:this.state.qty-1
+        })
     }
     render(){
         const {price , title , qty} =this.state;
@@ -22,12 +42,12 @@ class Cartitem extends React.Component {
                 </div>
                 <div className='right-block'>
                     <div style={{fontSize:25}}>{title}</div>
-                    <div style={{color:'#777'}}>{price}</div>
-                    <div style={{color:'#777'}}>{qty}</div>
+                    <div style={{color:'#777'}}>Rs. {price}</div>
+                    <div style={{color:'#777'}}>Qty :{qty}</div>
                     <div id='cart-item-actions'>
                         {/* {buttons} */}
                         <img alt='increase' className='action-icons' src='https://as2.ftcdn.net/v2/jpg/01/07/62/07/1000_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg' onClick={this.increaseQuanity.bind(this)}/>
-                        <img alt='decrease' className='action-icons' src='https://as1.ftcdn.net/v2/jpg/03/73/49/86/1000_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg' />
+                        <img alt='decrease' className='action-icons' src='https://as1.ftcdn.net/v2/jpg/03/73/49/86/1000_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg' onClick={this.decreaseQuantity.bind(this)}/>
                         <img alt='delete' className='action-icons' src='https://as2.ftcdn.net/v2/jpg/03/73/50/09/1000_F_373500918_7vISJB85YXvvu7SgnpktP752LWRrLzyI.jpg' />
                     </div>
                 </div>
